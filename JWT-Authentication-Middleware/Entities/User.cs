33d2899c9ffec76589c7a088c101bad0f6ce45fd
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-11-07 16:54:08 +0200

    User class and Models for Authentication request and response

---
+++ JWT-Authentication-Middleware/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace JWT_Authentication_Middleware.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
---
+++ JWT-Authentication-Middleware/Models/AuthenticateResponse.cs
@@ -0,0 +1,19 @@
+﻿using JWT_Authentication_Middleware.Entities;
+using System;
+
+namespace JWT_Authentication_Middleware.Models
+{
+    public class AuthenticateResponse
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponse(User user, string token)
+        {
+            Id = user.Id;
+            Username = user.Username;
+            Token = token;
+        }
+    }
+}
