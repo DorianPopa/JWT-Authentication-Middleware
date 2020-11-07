115c8c3efefe1662df0411e0d046f5079cc7a13a
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-11-07 17:08:35 +0200

    Basic UsersController structure

---
+++ JWT-Authentication-Middleware/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using JWT_Authentication_Middleware.Models;
+using Microsoft.Extensions.Logging;
+
+namespace JWT_Authentication_Middleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public UsersController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(AuthenticateRequest request)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
