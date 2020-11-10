d140145b2eee525433db974cabb2b9efccd010ba
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-11-10 03:50:06 +0200

    Added UserService and JwtMiddleware to Startup

--- JWT-Authentication-Middleware/Startup.cs
+++ JWT-Authentication-Middleware/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using JWT_Authentication_Middleware.Configuration;
+using JWT_Authentication_Middleware.Helpers;
+using JWT_Authentication_Middleware.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace JWT_Authentication_Middleware
 {
@@ -30,9 +25,11 @@ namespace JWT_Authentication_Middleware
             services.AddControllers();
 
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            services.AddScoped<IUserService, UserService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.  
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,13 +38,14 @@ namespace JWT_Authentication_Middleware
             }
 
             app.UseHttpsRedirection();
+
             app.UseRouting();
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
-            // The custom JWT middleware will go here
+            app.UseMiddleware<JwtMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
